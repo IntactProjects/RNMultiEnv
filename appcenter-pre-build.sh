cd89db2ee32f7bebbcd93790b7345b1d3316fe13
Aurelien Gustan <aurelien.gustan@in-tact.fr>
2019-10-07 23:07:35 +0200

    Fixed mistakes in appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -12,23 +12,22 @@ fi
 
 if [ "$APPCENTER_BRANCH" == "production" ];
 then
-    DEVOPS_SOURCE_FILENAME=devops.prod.json
+    DEVOPS_SOURCE_FILENAME=devops.production.json
     BUNDLE_DISPLAY_NAME_SUFFIX=
     BUILD_CONFIGURATION_NAME=Release
 fi
 
-    echo "Choosing devops.json"
-    mv $APPCENTER_SOURCE_DIRECTORY/devops.staging.json $APPCENTER_SOURCE_DIRECTORY/devops.json
+echo "Choosing devops.json"
+mv $APPCENTER_SOURCE_DIRECTORY/devops.$DEVOPS_SOURCE_FILENAME.json $APPCENTER_SOURCE_DIRECTORY/devops.json
 
-    echo "Updated devops.json"
-    cat $APPCENTER_SOURCE_DIRECTORY/devops.json
+echo "Updated devops.json"
+cat $APPCENTER_SOURCE_DIRECTORY/devops.json
 
-    echo "Updating CFBundleDisplayName in Info.plist"
-    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME)$BUNDLE_DISPLAY_NAME_SUFFIX" $INFO_PLIST_FILE
+echo "Updating CFBundleDisplayName in Info.plist"
+plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME)$BUNDLE_DISPLAY_NAME_SUFFIX" $INFO_PLIST_FILE
 
-    echo "Updated Info.plist:"
-    cat $INFO_PLIST_FILE
+echo "Updated Info.plist:"
+cat $INFO_PLIST_FILE
 
-    echo "Updating Build configuration"
-    sed -i -e 's/buildConfiguration = "Release"/buildConfiguration = "'$BUILD_CONFIGURATION_NAME'"/g' $APPCENTER_SOURCE_DIRECTORY/ios/$PROJECT_NAME.xcodeproj/xcshareddata/xcschemes/$PROJECT_NAME.xcscheme
-fi
+echo "Updating Build configuration"
+sed -i -e 's/buildConfiguration = "Release"/buildConfiguration = "'$BUILD_CONFIGURATION_NAME'"/g' $APPCENTER_SOURCE_DIRECTORY/ios/$PROJECT_NAME.xcodeproj/xcshareddata/xcschemes/$PROJECT_NAME.xcscheme
