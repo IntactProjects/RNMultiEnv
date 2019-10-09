c905d4063eb7a5ef622d8feb7455a7db457b12ca
Aurelien Gustan <aurelien.gustan@in-tact.fr>
2019-10-09 17:40:44 +0200

    Added some comments to the appcenter-pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -17,17 +17,22 @@ then
     BUILD_CONFIGURATION_NAME=Release
 fi
 
+# React Native: Choose the good devops.json file depending on the environment
 echo "Choosing devops.json"
 mv $APPCENTER_SOURCE_DIRECTORY/$DEVOPS_SOURCE_FILENAME $APPCENTER_SOURCE_DIRECTORY/devops.json
 
 echo "Updated devops.json"
 cat $APPCENTER_SOURCE_DIRECTORY/devops.json
 
+# iOS: Update the iOS app name
 echo "Updating CFBundleDisplayName in Info.plist"
 plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME)$BUNDLE_DISPLAY_NAME_SUFFIX" $INFO_PLIST_FILE
 
 echo "Updated Info.plist:"
 cat $INFO_PLIST_FILE
 
+# iOS: Build with the good build configuration
 echo "Updating Build configuration"
 sed -i -e 's/buildConfiguration = "Release"/buildConfiguration = "'$BUILD_CONFIGURATION_NAME'"/g' $APPCENTER_SOURCE_DIRECTORY/ios/$PROJECT_NAME.xcodeproj/xcshareddata/xcschemes/$PROJECT_NAME.xcscheme
+
+# Android: The build type is chosen directly from MS App Center
